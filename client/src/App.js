72fa2be3c4d3eccf56b98ddcb13b86d66c20134a
Julian Wagner <wagnerjw92@gmail.com>
2020-10-12 14:53:58 -0700

    started building out react front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
+
 
 function App() {
+
+  const [dogs, setDogs] = useState([]);
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [age, setAge] = useState("");
+
+  const fetchDogs = () => {
+    axios.get("http://localhost:8000/api/dogs")
+      .then(res => {
+        console.log(res);
+        setDogs(res.data);
+      })
+      .catch(err => console.log(err));
+  }
+
+  const addDog = (e) => {
+    e.preventDefault();
+    const newDog = {name, age, breed};
+    axios.post("http://localhost:8000/api/dogs", newDog)
+      .then(res => {
+        console.log(res);
+        fetchDogs();
+        setName("");
+        setBreed("");
+        setAge("");
+      })
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    fetchDogs();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container"></div>
+
+        <h1>Disco Dogs</h1>
+
+        <div>
+          {dogs.map( dog => 
+            <div key={dog.id}>
+              <h4>Name: { dog.name }</h4>
+              <p>Breed: { dog.breed } </p>
+              <p>Age: { dog.age }</p>
+            </div>
+            )}
+        </div>
+
+        <form onSubmit={ addDog }>
+          <p>Name:  <input type="text" onChange={ e => setName(e.target.value) } value={ name } /> </p>
+          <p>Breed: <input type="text" onChange={ e => setBreed(e.target.value)} value={ breed } /> </p>
+          <p>Age:   <input type="number" onChange={ e => setAge(e.target.value) } value={ age }/> </p>
+          <input type="submit" />
+        </form>
+
     </div>
   );
 }
